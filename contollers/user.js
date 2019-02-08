9f7ad00bcaa4587a1a17aa3878324b59f051e7ba
David whyte <kingstonwhyte87@gmail.com>
2019-02-08 18:12:42 +0100

    add tests

--- contollers/user.js
+++ contollers/user.js
@@ -1,8 +1,9 @@
 import User from '../models/User';
 
-const create = () => {
+const create = (req, res) => {
   User.findAll().then((users) => {
     console.log(users);
+    res.status(200).json({ status: 200 });
   });
 };
 
