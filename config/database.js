496851efe4f60b406fa475f000910a6f21835051
David whyte <kingstonwhyte87@gmail.com>
2019-02-11 16:27:55 +0100

    update travis

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 
+
 dotenv.config();
 
 console.log(process.env.DB_URL);
 const sequelize = new Sequelize(process.env.DB_URL, { dialect: 'postgres' });
 
-sequelize.sync();
+sequelize.authenticate()
+  .then(() => { console.log('Database connected'); })
+  .catch((error) => { console.log(error); });
+sequelize.sync().then(() => console.log('juu'));
 
 module.exports = sequelize;
