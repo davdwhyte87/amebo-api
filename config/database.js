e4293673bebfd1d5d546fc25bf50dabfc33d9451
David whyte <kingstonwhyte87@gmail.com>
2019-02-11 17:27:03 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ console.log(process.env.DB_URL);
 const sequelize = new Sequelize(process.env.DB_URL, { dialect: 'postgres' });
 
 const User = UserModel(sequelize);
-sequelize.sync().then(() => console.log('juu'));
+sequelize.sync();
 sequelize.authenticate()
   .then(() => { console.log('Database connected'); })
   .catch((error) => { console.log(error); });
