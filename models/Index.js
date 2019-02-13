aade7649a9f5587c5b205e805904633c621c11cc
David whyte <kingstonwhyte87@gmail.com>
2019-02-13 09:27:11 +0100

    add model configuration

--- models/Index.js
+++ models/Index.js
@@ -1,19 +1,23 @@
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
 
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = path.join(__dirname, '/../config/config.json')[env];
+dotenv.config();
+const basename = path.basename(module.filename);
+// const env = process.env.NODE_ENV || 'development';
+// const config = path.join(__dirname, './config/config.json')[env];
+
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env.config.use_env_variable, config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
+const sequelize = new Sequelize(process.env.DB_URL, { dialect: 'postgres' });
 fs
   .readdirSync(__dirname)
   .filter((file) => {
